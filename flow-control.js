c7b47a0181c1318b74afaa8d77a1540b1e5fc5f7
Michael Petan <mpetan2012@gmail.com>
2020-12-31 21:59:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,15 @@
 function basicTeenager(age) {
-
+  if (age >= 21) {
+    return "Congrats you can drink!"
+  } else {
+    return "You'll be there soon enough!"
+  }
 }
 
 function teenager(age) {
-
+  if (age === 20){
+    return "One more year left!"
+  }
 }
 
 function ageChecker(age) {
